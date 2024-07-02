f3f1ac56c025dc90be9ac268c8ef4b0dac4fd4ec
sha512sum <linuxnyasha@tutanota.com>
2024-07-02 16:28:32 +0000

    Add logger

---
+++ include/cserver/components/loggable_component_base.hpp
@@ -0,0 +1,24 @@
+#pragma once
+#include <cserver/components/logger.hpp>
+#include <cserver/engine/components.hpp>
+
+namespace cserver {
+
+struct ComponentBase {
+  Logging& logging;
+  template <typename T>
+  constexpr ComponentBase(T& context) : 
+      logging(context.template FindComponent<"logging">()) {};
+};
+
+
+#define LOG_TRACE   if(this->logging.level == ::cserver::LoggingLevel::kTrace) this->logging.Trace
+#define LOG_DEBUG   if(this->logging.level <= ::cserver::LoggingLevel::kDebug) this->logging.Debug
+#define LOG_INFO    if(this->logging.level <= ::cserver::LoggingLevel::kInfo) this->logging.Info
+#define LOG_WARNING if(this->logging.level <= ::cserver::LoggingLevel::kWarning) this->logging.Warning
+#define LOG_ERROR   if(this->logging.level <= ::cserver::LoggingLevel::kError) this->logging.Error
+
+
+
+
+} // namespace cserver
